b5a406d791a0ecaa3a588fb7ef14d1d2c3d33284
Charles Allen <charles@allen-net.com>
2016-02-09 15:17:48 -0800

    get rid of no longer needed test

--- src/test/scala/io/druid/indexer/spark/TestScalaBatchIndexTask.scala
+++ src/test/scala/io/druid/indexer/spark/TestScalaBatchIndexTask.scala
@@ -257,8 +257,4 @@ class TestScalaBatchIndexTask extends FlatSpec with Matchers
 
     task1 should not equal buildSparkBatchIndexTask(classpathPrefix = "someOther.jar")
   }
-
-  "The scala version matchers" should "properly parse the version" in {
-    SparkBatchIndexTask.SCALA_VERSION should be("2.10")
-  }
 }
