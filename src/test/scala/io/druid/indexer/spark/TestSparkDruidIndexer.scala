d1d27319726a1dc9423653f5f333b6d5b890748c
Charles Allen <charles@allen-net.com>
2016-05-04 12:02:39 -0700

    Fix misspelling

--- src/test/scala/io/druid/indexer/spark/TestSparkDruidIndexer.scala
+++ src/test/scala/io/druid/indexer/spark/TestSparkDruidIndexer.scala
@@ -127,7 +127,7 @@ class TestSparkDruidIndexer extends FlatSpec with Matchers
                   dv should not be null
                   // I had a problem at one point where dimension values were being stored as lists
                   // This is a check to make sure the dimension is a list of values rather than being a list of lists
-                  // If the unit test is ever modified to have dimenions values that start with this offending case
+                  // If the unit test is ever modified to have dimension values that start with this offending case
                   // then of course this test will fail.
                   dv should not startWith "List("
                   dv should not startWith "Set("
