20821daecb02937872696194ea85c2eda54c09d1
Jisoo Kim <jisoo.kim@metamarkets.com>
2017-08-10 13:30:25 -0700

    use druid.java.util.common.lifecycle.Lifecycle (#109)
    * use druid.java.util.common.lifecycle.Lifecycle
    
    * change all metamx.common to druid.java.util.common

--- src/main/scala/io/druid/indexer/spark/SparkDruidIndexer.scala
+++ src/main/scala/io/druid/indexer/spark/SparkDruidIndexer.scala
@@ -23,13 +23,24 @@ import java.io._
 import java.nio.file.Files
 import java.util
 
+import com.esotericsoftware.kryo.io.{Input, Output}
+import com.esotericsoftware.kryo.{Kryo, KryoSerializable}
+import com.fasterxml.jackson.core.`type`.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.common.io.Closer
+import com.google.inject.name.Names
+import com.google.inject.{Binder, Injector, Key, Module}
+import com.metamx.emitter.service.{ServiceEmitter, ServiceMetricEvent}
 import io.druid.data.input.MapBasedInputRow
 import io.druid.data.input.impl._
-import io.druid.guice.{GuiceInjectors, JsonConfigProvider}
 import io.druid.guice.annotations.{Json, Self}
+import io.druid.guice.{GuiceInjectors, JsonConfigProvider}
 import io.druid.indexer.HadoopyStringInputRowParser
 import io.druid.initialization.Initialization
+import io.druid.java.util.common.{IAE, ISE}
 import io.druid.java.util.common.granularity.Granularity
+import io.druid.java.util.common.lifecycle.Lifecycle
+import io.druid.java.util.common.logger.Logger
 import io.druid.query.aggregation.AggregatorFactory
 import io.druid.segment._
 import io.druid.segment.column.ColumnConfig
@@ -39,24 +50,13 @@ import io.druid.segment.loading.DataSegmentPusher
 import io.druid.server.DruidNode
 import io.druid.timeline.DataSegment
 import io.druid.timeline.partition.{HashBasedNumberedShardSpec, NoneShardSpec, ShardSpec}
-import com.esotericsoftware.kryo.{Kryo, KryoSerializable}
-import com.esotericsoftware.kryo.io.{Input, Output}
-import com.fasterxml.jackson.core.`type`.TypeReference
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.google.common.io.Closer
-import com.google.inject.{Binder, Injector, Key, Module}
-import com.google.inject.name.Names
-import com.metamx.common.{IAE, ISE}
-import com.metamx.common.lifecycle.Lifecycle
-import com.metamx.common.logger.Logger
-import com.metamx.emitter.service.{ServiceEmitter, ServiceMetricEvent}
 import org.apache.commons.io.FileUtils
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
-import org.apache.spark.{Partitioner, SparkContext}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.scheduler.{AccumulableInfo, SparkListener, SparkListenerApplicationEnd, SparkListenerStageCompleted}
 import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{Partitioner, SparkContext}
 import org.joda.time.{DateTime, Interval}
 
 import scala.collection.JavaConversions._
